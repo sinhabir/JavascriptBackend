2e5ade463aae84d643af74144edc73cbf7eb90b6
sinhabir <abir.sinha09@gmail.com>
2024-03-03 19:48:45 +0100

    Added getuserTweet code on TweetController

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -29,16 +29,92 @@ const createTweet = asyncHandler(async (req, res) => {
 })
 
 //***********************************/
-// TODO: get user tweets
+// Get user tweets
 const getUserTweets = asyncHandler(async (req, res) => {
-    
+    const { userId } = req.params
+
+    if(!isValidObjectId(userId)){
+        throw new ApiError(400, "User Id is not valid")
+    }
+
+    //fetch tweet details along with like count and Owner details
+    const tweets = await Tweet.aggregate([
+        {
+            $match:{
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "ownerDetails",
+                pipeline: [
+                    {
+                        $project:{
+                            username: 1,
+                            avatar: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $lookup:{
+                from: "likes",
+                localField: "_id",
+                foreignField:"tweet",
+                as:"likeDetails",
+                pipeline:[
+                    {
+                        $project:{
+                            likedBy: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields:{
+                likesCount:{
+                    $size: "$likeDetails"
+                },
+                ownerDetails:{
+                    $first: "$ownerDetails"
+                }
+
+            }
+        },
+        {
+            $sort:{
+                createdAt :-1
+            }
+        },
+        {
+            $project:{
+                content: 1,
+                ownerDetails: 1,
+                likesCount: 1,
+                createdAt: 1
+            }
+        }
+    ])
+
+    if(!tweets){
+        throw new ApiError(500, "Error while fetching tweet details")
+    }
+
+    res.status(200)
+    .json(new ApiResponse(200, tweets, "Tweets fetched succesfully"))
+
 })
 
 //***********************************/
  // update tweet
 const updateTweet = asyncHandler(async (req, res) => {
    const { content } = req.body
-   const { tweetId } = req. params
+   const { tweetId } = req.params
 
    if(!content){
     throw new ApiError(400, "Content is required to update")
